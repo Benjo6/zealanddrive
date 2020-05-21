0e7f99bf2effa294ab5515a7d71618a5682c58c1
Michelle Kristensen <mich5h03@edu.easj.dk>
2020-05-21 11:22:23 +0200

    combobox virker ikke

--- ZealandDrive/ZealandDrive/VM/UserVM.cs
+++ ZealandDrive/ZealandDrive/VM/UserVM.cs
@@ -38,7 +38,7 @@ namespace ZealandDrive.VM
         #region Constructor
         public UserVM()
         {
-            _createOne2 = new RelayCommand(AddUser);
+            _createOne2 = new RelayCommand(AddUser1);
             p = new PageCommand();
             _loadUser = new RelayCommand(LoadUsers);
             _userToBeCreated = new Users();
@@ -50,7 +50,6 @@ namespace ZealandDrive.VM
             _persistence = PersitenceFactory.GetPersistency(PersistenceType.Database);
             _users = new ObservableCollection<Users>();
             //_saveUser = new RelayCommand(SaveMethod);
-
         }
         #endregion
         #region Properties
@@ -146,7 +145,7 @@ namespace ZealandDrive.VM
             f.Navigate(typeof(LoginPage));
 
         }
-        private async void AddUser()
+        private async void AddUser1()
         {
 
             //todo give error message
