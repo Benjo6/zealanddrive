606caabbf1bc533f1fbc738e0d7cd7c4a73f3f40
taxidriver2192 <56368331+taxidriver2192@users.noreply.github.com>
2020-05-21 14:05:14 +0200

    Tilført noget til User fra Datamatiker-1.-semester-2019-master
    Dette er der muligvist nogle fejl i, men tilføre det fra michelles gamle projekt sådan at man muligvist kan lave login.

--- ZealandDrive/ClassLibrary/Users.cs
+++ ZealandDrive/ClassLibrary/Users.cs
@@ -17,17 +17,11 @@ namespace ClassLibrary
         private string _token;
 
         public string name { get => _name; set => _name =value ; }
-
         public string lastname { get=> _lastName; set => _lastName = value; }
-
         public string email { get => _email; set => _email = value; }
-
         public string password { get => _password; set => _password = value; }
         public string tmpPassword { get => _password; set => _password = value; }
-        // her vælger man at krypter passwordet ved at tage passwordet og lave det om til exkempelvis HMAC kryptering.
         public string token { get => _password; set => _password = value; }
-        // Her vælger man at lave et token der bliver ved med at sendes rundt i header filen, sådan at man ved hvem der er logget ind. 
-        // Hvis brugeren er logget ud bliver token fjernet i databasen og derfor at burgeren ikke loget ind mere, selv om han har sin gamle token kode.
 
         public int id { get => _id; set => _id = value; }
 
@@ -45,6 +39,14 @@ namespace ClassLibrary
             _password = password;
             _tmpPassword = tmpPassword;
             _token = token;
+        }
+
+        public bool CheckInformation()
+        {
+            if (!email.Equals("") && !password.Equals(""))
+                return true;
+            else
+                return false;
         }
 
         public override string ToString()
